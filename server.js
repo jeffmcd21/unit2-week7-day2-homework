8f736aeb17f1ba9fb95ca82d6522b521e45835c6
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-16 17:46:33 -0500

    Setup database and send json

--- server.js
+++ server.js
@@ -8,16 +8,23 @@ const morgan = require("morgan")
 // Import method override
 const methodOverride = require("method-override")
 
+// Import the drinks
+// Set variable for module.exports
+const drinks = require("./models/drinks.js")
 
 // Create app object
 const port = express()
 
-
-
+// Set GET Route
 port.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!")
 })
 
+
+// Set Variable for Drinks
+port.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 
 
 
